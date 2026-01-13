e41f607894e2f826054c1039e8f2b6e8c7cd7ba8
pricingxol <pricingxol@gmail.com>
2026-01-13 09:22:29 +0700

    Delete loader.py

--- loader.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import streamlit as st
-
-@st.cache_data
-def load_excel(path):
-    xls = pd.ExcelFile(path)
-    sheets = {s: pd.read_excel(xls, s) for s in xls.sheet_names}
-    return sheets
